68dc73174e3793a045a3bfcf1d8702934bfc46cc
SDexh <wh0s_on_first@yahoo.com>
2014-02-18 19:23:32 -0700

    Code
    Have some

--- print.c
+++ print.c
@@ -5,8 +5,6 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[]);
 
 void print_line(char line[], char source_name_to_print[], char date_to_print[])
 {
-    char save_c;
-    char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
     
     if (++line_count > MAX_LINES_PER_PAGE)
@@ -14,22 +12,28 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
         print_page_header(source_name_to_print, date_to_print);
 		line_count = 0;
     }
+
+	printf("%d\t", line_count);
+
     if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
     {
-		int ti = strlen(line) - MAX_PRINT_LINE_LENGTH;
-		char Temp[ti];
-        /* Missing Code Here */
+		char Temp[MAX_PRINT_LINE_LENGTH], *tptr;
+		int i;
+		for(i = 0; i < MAX_PRINT_LINE_LENGTH - 1; i++)
+		{
+			Temp[i] = line[i];
+		}
+		Temp[i] = '\0';
+		printf("%s\n", Temp);
+		tptr = &line[i];
+		print_line(tptr, source_name_to_print, date_to_print);
     }
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
-    /* Missing Code Here */
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
+	else
+	{
+		printf("%s\n", line);
+	}
 }
+
 static void print_page_header(char source_name[], char date[])
 {
     static int page_number = 0;
