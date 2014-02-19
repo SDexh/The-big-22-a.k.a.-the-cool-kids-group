559c36fd75147f545d691d13d49a6930c22f9da4
SDexh <wh0s_on_first@yahoo.com>
2014-02-18 21:36:02 -0700

    Implementation
    init_lister is now implemented

--- main.c
+++ main.c
@@ -12,24 +12,36 @@ int main (int argc, const char *argv[])
 	
     return 0;
 }
+
+//creates the file stream and the name and date to appropriate arrays
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
 {
+	//used for getting the time in string format
     time_t timer, *timep;
 	struct tm *timetm;
+	//stores the file to be opened
     FILE *file;
+	//index
 	int i = 0;
+	//creates a pointer for timer
 	timep = &timer;
+	//converts it into a usable format
 	timetm = localtime(timep);
-	strftime(dte, 11, "%x", timetm);
-	while(*name != '\n')
+	//writes the date in string format to the date array
+	strftime(dte, DATE_STRING_LENGTH, "%x", timetm);
+	//copies name to sourcefilename
+	while(*name != '\0')
 	{
 		source_file_name[i] = *name;
 		name++;
 		i++;
 	}
+	//creates the filestream
 	file = fopen(source_file_name, "r");
+	//returns the file stream
     return file;
 }
+
 BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
 {
     char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
