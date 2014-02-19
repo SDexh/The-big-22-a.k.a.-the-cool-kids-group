5a98b66465ca2eea17d4691bdbeb0596f6ba5c95
SDexh <wh0s_on_first@yahoo.com>
2014-02-18 18:41:11 -0700

    print.c start
    started the code

--- print.c
+++ print.c
@@ -1,19 +1,23 @@
 #include "common.h"
 
 static void print_page_header(char source_name[], char date[]);
+void print_line(char line[], char source_name_to_print[], char date_to_print[]);
 
 void print_line(char line[], char source_name_to_print[], char date_to_print[])
 {
-    char save_ch;
+    char save_c;
     char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
     
     if (++line_count > MAX_LINES_PER_PAGE)
     {
-        /* Missing Code Here */
+        print_page_header(source_name_to_print, date_to_print);
+		line_count = 0;
     }
     if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
     {
+		int ti = strlen(line) - MAX_PRINT_LINE_LENGTH;
+		char Temp[ti];
         /* Missing Code Here */
     }
     if (save_chp)
