356d89bb462f95dae8b990524ec7534db8b5e719
SDexh <wh0s_on_first@yahoo.com>
2014-02-19 19:58:47 -0700

    Edits
    Some edits in get_source_line and main made.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main (int argc, const char *argv[])
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     
-    source_file = init_lister(argv, source_name, date);
+    source_file = init_lister(*argv, source_name, date);
 	
     return 0;
 }
@@ -42,21 +42,31 @@ FILE *init_lister(const char *name, char source_file_name[], char dte[])
     return file;
 }
 
+//gets the line to print and calls print line
 BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
 {
     char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
     char source_buffer[MAX_SOURCE_LINE_LENGTH];
     static int line_number = 0;
-    
-    if (fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file) != NULL)
+	//initializes the first element in the array to be null, this is used to check the success of fgets
+	source_buffer[0] = '\0';
+	//attempts to copy the string from the stream
+    fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file);
+	//if the first char is not the null char
+    if (source_buffer[0])
     {
-      print_buffer = sprintf(source_buffer, "%s", line_number);
-       line_number++;
-       print_line(char line[], char source_name_to_print[], char date_to_print[]);
+		//copies the string from the source buffer to the print buffer
+		strcpy(print_buffer, source_buffer);
+		//increments the line number
+		line_number++;
+		//calls print line
+		print_line(print_buffer, src_name, todays_date);
+		//rturns true
         return (TRUE);
     }
     else
     {
+		//returns false
         return (FALSE);
     }
 }
